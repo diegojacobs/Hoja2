703d5eaa05c0d9ccbecd8cecf4d14020befb392f
RobertoC27 <chi13027@uvg.edu.gt>
2014-07-26 15:42:55 -0600

    leer archivo
    ya esta arreglado el leer archivo

--- src/hoja2/LeerArchivo.java
+++ src/hoja2/LeerArchivo.java
@@ -12,5 +12,24 @@ import java.io.*;
  * @author Usuario
  */
 public class LeerArchivo {
-    
+    public static void main(String args[]) {
+        try{
+            // Abrimos el archivo
+            FileInputStream fstream = new FileInputStream("pr.txt");
+            // Creamos el objeto de entrada
+            DataInputStream entrada = new DataInputStream(fstream);
+            // Creamos el Buffer de Lectura
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+            String strLinea;
+            // Leer el archivo linea por linea
+            while ((strLinea = buffer.readLine()) != null)   {
+                // Imprimimos la línea por pantalla
+                System.out.println (strLinea);
+            }
+            // Cerramos el archivo
+            entrada.close();
+        }catch (Exception e){ //Catch de excepciones
+            System.err.println("Ocurrio un error: " + e.getMessage());
+        }
+    }
 }
