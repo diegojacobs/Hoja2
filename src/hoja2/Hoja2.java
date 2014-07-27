a00b45d5ba9f41c2d414e4c88ae7ecdaefa4ac05
Usuario <Usuario@VAIO>
2014-07-26 20:59:18 -0600

    Postfix

--- src/hoja2/Hoja2.java
+++ src/hoja2/Hoja2.java
@@ -20,10 +20,57 @@ public class Hoja2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        myStack st;
+        myStack st = new myStack();
+        LeerArchivo entrada = new LeerArchivo();
+        Evaluar check = new Evaluar();
+        String expr;
+        int num, num2;
+        System.out.print("La expresión es: ");
+        System.out.print(entrada.postfix());
         
+        expr = entrada.postfix();
         
-        
+        for (int i=1;i<=expr.length();i++)
+        {
+            String car;
+            car = expr.substring(i-1, i);
+            if("num".equals(check.operacion(car))) 
+            {
+                num = Integer.parseInt(car);
+                st.push(num);
+            }
+            if ("resta".equals(check.operacion(car)))
+            {
+                num=st.pop();
+                num2=st.pop();
+                num=num2-num;
+                st.push(num);
+            }
+            if ("suma".equals(check.operacion(car)))
+            {
+                num=st.pop();
+                num2=st.pop();
+                num=num+num2;
+                st.push(num);
+            }
+            if ("por".equals(check.operacion(car)))
+            {
+                num=st.pop();
+                num2=st.pop();
+                num=num*num2;
+                st.push(num);
+            }
+            if ("dividir".equals(check.operacion(car)))
+            {
+                num=st.pop();
+                num2=st.pop();
+                num=num2/num;
+                st.push(num);
+            }
+        }
+        num=st.pop();
+        System.out.print("\nEl resultado es: ");
+        System.out.print(num);
     }
     
 }
